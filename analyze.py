4b29cdd60a0f8b9b8d0ae92932c5d59afebc243d
Parker Wild <parkerwild96@gmail.com>
2023-05-04 01:55:07 -0400

    Implements LMP valuation

--- analyze.py
+++ analyze.py
@@ -24,45 +24,105 @@ here = os.path.dirname(os.path.realpath(__file__))
 #--- DATA ---#
 
 # Distributed generation interconnection program data for California.
-site_file = os.path.join(here, 'data/Interconnected_Project_Sites_2023-03-31/CAISO_Interconnected_Project_Sites_2023-03-31.csv')
+site_file = os.path.join(here, 'data/Interconnected_Project_Sites_2023-03-31/Aggregated_Sites.csv')
 
-# Directory of representative generation profiles from NREL's System Advisor Model (SAM). 
-sam_dir = os.path.join(here, 'data/SAM_Generation_Profiles/')
+# Representative generation profiles from NREL's System Advisor Model (SAM). 
+gen_file = os.path.join(here, 'data/SAM_Generation_Profiles/Aggregated_Profiles.csv')
 
 # Locational marginal prices at default Load Aggregation Points (LAPs) in 2022 from CAISO. 
 lmp_file = os.path.join(here, 'data/CAISO_LMPs/2022/DLAP_PGAE-APND, DLAP_SCE-APND, DLAP_SDGE-APND/Aggregated_LMPs.csv')
 
 #--- OUTPUT ---# 
 
-# Revenue under NEM. 
-nem_rev_file = os.path.join(here, 'output/nem_rev.csv')
+# Value of solar generation under NEM. 
+nem_vosg_file = os.path.join(here, 'output/nem_vosg.csv')
 
-# Revenue under LMPs. 
-lmp_rev_file = os.path.join(here, 'output/rev_lmp.csv')
+# Value of solar under LMPs. 
+lmp_vosg_file = os.path.join(here, 'output/lmp_vosg.csv')
 
 #--- TARIFFS ---#
 
-# TODO: Data structures for each utility's version of NEM 1.0 and 2.0.
+# TODO: Data structures for each utility's version of NEM (1.0 and 2.0).
 nems = {
      'PGE': {},
      'SCE': {},
      'SDGE': {}
 }
 
+#--- PROFILES ---#
+
+# Profile names. 
+profile_names = [
+     'PGE_FIXED_ROOFTOP_T18_A180_NORM',
+     'SCE_FIXED_ROOFTOP_T18_A180_NORM', 
+     'SDGE_FIXED_ROOFTOP_T18_A180_NORM'
+]
+
+#--- CROSSWALKS ---#
+
+# From region to node.
+region_to_node = {
+     'PGE': 'DLAP_PGAE-APND',
+     'SCE': 'DLAP_SCE-APND',
+     'SDGE': 'DLAP_SDGE-APND'
+}
+
 #--- FUNCTIONS ---#
 
-def nem():
-     '''Calculate value under NEM tariffs.'''
+def valuation(sites, profiles, lmps):
+     '''Calculate value of solar generation under NEM and LMPs.'''
 
-     return
+     # Create values dataframe.
+     values = profiles.copy()
 
-def lmp():
-     '''Calculate value under LMP.'''
-     
-     return
+     for profile in profile_names: 
+
+          # Region.
+          region = profile.split('_')[0]
+
+          # Column names. 
+          nem_rate_col = region + ' NEM Rate ($/kW)'
+          lmp_rate_col = region + ' LMP Rate ($/kW)'
+          nem_val_col = region + ' Normalized NEM Value'
+          lmp_val_col = region + ' Normalized LMP Value'
+
+          #--- Add NEM Rate ---#
+
+          # TODO: NEM
+
+          #--- Add LMP Rate ---#
+
+          # Identify nearest node to region.
+          node_id = region_to_node[region]
+          
+          # Get LMPs for nearest node.
+          lmp_slice = lmps.loc[(lmps['NODE_ID'] == node_id) & (lmps['LMP_TYPE'] == 'LMP')]
+
+          # Format LMPs. 
+          lmp_slice[lmp_rate_col] = lmp_slice['MW'] / 1000
+
+          # Drop unused columns. 
+          lmp_slice = lmp_slice[['Timestamp', lmp_rate_col]]
+
+          # Add column for LMPs. 
+          values = values.merge(lmp_slice, on='Timestamp')
+
+          #--- Normalized Values ---#
+
+          # TODO: Compute normalized value of solar under NEM.
+          # values[nem_val_col] = values[profile] * values[nem_rate_col]
+
+          # Compute normalized value of solar under LMPs.
+          values[lmp_val_col] = values[profile] * values[lmp_rate_col]
+
+     return values
 
 if __name__ == '__main__':
-    
+
+     #################
+     ### LOAD DATA ###
+     #################
+
      #--- Solar PV Sites ---#
 
      sites = None
@@ -71,30 +131,15 @@ if __name__ == '__main__':
           sites = pd.read_csv(site_file)
      else: 
           raise Exception('Please get site data using sites.ipynb.')
-
-     print(sites)
-
-     #--- Generation Profiles ---#
-
-     # Dictionary of generation profiles.
-     profiles = {}
-
-     for file in os.listdir(sam_dir):
      
-          # Read file.
-          path = os.path.join(sam_dir, file)
-          df = pd.read_csv(path, header=0, names=['Timestamp', 'kW'], encoding='utf-8')
+     #--- Representative Generation Profiles ---#
 
-          # Convert to datetime.
-          df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%b %d, %I:%M %p')
+     profiles = None
 
-          # Fix year.
-          df['Timestamp'] = df['Timestamp'].apply(lambda dt: dt.replace(year=2022))
-
-          # Add to dictionary.
-          profiles[file] = df
-     
-     print(profiles)
+     if os.path.exists(gen_file):
+          profiles = pd.read_csv(gen_file)
+     else:
+          raise Exception('Please get generation data using generation.ipynb.')
 
      #--- Locational Marginal Prices ---#
 
@@ -103,6 +148,12 @@ if __name__ == '__main__':
      if os.path.exists(lmp_file):
           lmps = pd.read_csv(lmp_file)
      else: 
-          raise Exception('Please get locational marginal price data using lmps.ipynb.')
+          raise Exception('Please get pricing data using lmps.ipynb.')
+          
+     ###########################################
+     ### CALCULATE VALUE OF SOLAR GENERATION ###
+     ###########################################
 
-     print(lmps)
+     # Calculate value of solar generation.
+     values = valuation(sites, profiles, lmps)
+     
